e51df736f63a1fa790eee621eb3fa5a4ee1551be
xorrr <derxor@gmail.com>
2013-11-25 13:56:12 +0100

    put the methods in notes_test.py into a class

--- notes_test.py
+++ notes_test.py
@@ -2,8 +2,9 @@ from webtest import TestApp
 
 import notes
 
-def test_index():
-    bottle = TestApp(notes.app)
-    result = bottle.get('/')
-    assert result.status == '200 OK'
-    assert result.body == 'hello'
+class TestWebserver():
+    def test_index(self):
+        bottle = TestApp(notes.app)
+        result = bottle.get('/')
+        assert result.status == '200 OK'
+        assert result.body == 'hello'
