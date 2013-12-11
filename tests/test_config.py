330969f2acc84ff61b2857eeaeb71f313f4048a4
xorrr <derxor@gmail.com>
2013-12-11 18:18:31 +0100

    add check if the config file uses a header

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,16 @@
 import os.path
+import ConfigParser
 
 CONFIG_FILE = 'user.cfg'
 
 class TestConfigFile():
     def test_if_config_exists(self):
         assert os.path.isfile(CONFIG_FILE), "config file does not exist"
+
+    def test_if_config_header_exists(self):
+        config = ConfigParser.RawConfigParser()
+        try:
+            config.read(CONFIG_FILE)
+            assert True
+        except ConfigParser.MissingSectionHeaderError:
+            assert False, "the config file does not use a header part"
